4f709c9d6a45301d4b5eeb00194c0936d90624fa
Dóra Kékesi <dorqa95@gmail.com>
2025-06-09 23:18:09 +0200

    npm package and gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const autoprefixer = require('autoprefixer');
 const log = require('fancy-log');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const cleancss = require('gulp-clean-css');
+const gulpCleanCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
-const gcmq = require('gulp-group-css-media-queries');
+const gulpGroupCssMediaQueries = require('gulp-group-css-media-queries');
 const livereload = require('gulp-livereload');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
@@ -13,7 +13,6 @@ const sourcemaps = require('gulp-sourcemaps');
 const gulpStylelint = require('gulp-stylelint');
 const stylus = require('gulp-stylus');
 const terser = require('gulp-terser');
-const watch = require('gulp-watch');
 const poststylus = require('poststylus');
 const rucksack = require('rucksack-css');
 
@@ -26,36 +25,30 @@ const paths = {
     htmls: [`${dest}**/*.html`, '!node_modules/**/*']
 };
 
-gulp.task('vendorjs', () =>
-    gulp
+function vendorJs() {
+    return gulp
         .src([
             'node_modules/babel-polyfill/dist/polyfill.js',
             `${source}vendor/**/*.js`
-        ])
+        ], { encoding: false })
         .pipe(concat('vendor.min.js'))
         .pipe(terser())
         .pipe(gulp.dest(`${dest}assets/js`))
-        .on('error', e => {
-            log.error(colors.red(`[ERROR] VendorJS was failed: ${e}`));
-        })
-);
+        .on('error', e => log.error(colors.red(`[ERROR] VendorJS was failed: ${e}`)));
+}
 
-gulp.task('vendorcss', () =>
-    gulp
-        .src([`${source}vendor/**/*.css`])
+function vendorCss() {
+    return gulp
+        .src([`${source}vendor/**/*.css`], { encoding: false })
         .pipe(concat('vendor.min.css'))
-        .pipe(
-            cleancss()
-        )
+        .pipe(gulpCleanCss())
         .pipe(gulp.dest(`${dest}assets/css`))
-        .on('error', e => {
-            log.error(colors.red(`[ERROR] VendorCSS was failed: ${e}`));
-        })
-);
+        .on('error', e => log.error(colors.red(`[ERROR] VendorCSS was failed: ${e}`)));
+}
 
-gulp.task('js', () =>
-    gulp
-        .src(`${source}js/*.js`)
+function js() {
+    return gulp
+        .src(`${source}js/*.js`, { encoding: false })
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(concat('main.min.js'))
@@ -63,7 +56,7 @@ gulp.task('js', () =>
             babel({
                 presets: [
                     [
-                        'env',
+                        '@babel/preset-env',
                         {
                             targets: {
                                 browsers: [
@@ -80,33 +73,27 @@ gulp.task('js', () =>
         .pipe(terser())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${dest}assets/js`))
-        .on('error', e => {
-            log.error(colors.red(`[ERROR] JS build was failed: ${e}`));
-        })
-        .pipe(livereload())
-);
+        .on('error', e => log.error(colors.red(`[ERROR] JS build was failed: ${e}`)))
+        .pipe(livereload());
+}
 
-gulp.task('lint', () =>
-    gulp
+function lint() {
+    return gulp
         .src([`${source}css/layout/*.styl`, `${source}css/modules/**/*.styl`])
         .pipe(gulpStylelint({
-            reporters: [
-                {formatter: 'string', console: true}
-            ]
-        }))
-);
+            reporters: [{ formatter: 'string', console: true }]
+        }));
+}
 
 const processors = [
-    autoprefixer({
-        cascade: false
-    }),
+    autoprefixer({ cascade: false }),
     rucksack,
-    gcmq
+    gulpGroupCssMediaQueries
 ];
 
-gulp.task('css', () =>
-    gulp
-        .src(`${source}css/style.styl`)
+function css() {
+    return gulp
+        .src(`${source}css/style.styl`, { encoding: false })
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(
@@ -116,41 +103,42 @@ gulp.task('css', () =>
             })
         )
         .pipe(rename({ suffix: '.min' }))
-        .pipe(
-            cleancss()
-        )
+        .pipe(gulpCleanCss())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${dest}assets/css`))
-        .on('error', e => {
-            log.error(colors.red(`[ERROR] CSS build was failed: ${e}`));
-        })
-        .pipe(livereload())
-);
+        .on('error', e => log.error(colors.red(`[ERROR] CSS build was failed: ${e}`)))
+        .pipe(livereload());
+}
 
-/* Watchers */
-gulp.task('watch', () => {
-    watch(`${source}css/**/*.styl`, gulp.series('css'));
-
-    watch(`${source}js/**/*.js`, gulp.series('js'));
-
-    watch(`${dest}assets/css/*.css`, file => {
-        log.info(colors.blue('[INFO] Style has been changed!'));
-        livereload.changed(file.path);
-    });
-
-    watch(paths.htmls, file => {
-        log.info(colors.green('[INFO] HTML has been changed!'));
-        livereload.changed(file.path);
-    });
-
-    watch(paths.phps, file => {
-        log.info(colors.yellow('[INFO] PHP has been changed!'));
-        livereload.changed(file.path);
-    });
-
+function watchFiles() {
     livereload.listen();
-});
 
-/* Default task */
-gulp.task('buildVendors', gulp.parallel('vendorcss', 'vendorjs'));
-gulp.task('default', gulp.parallel('css', 'js'));
+    gulp.watch(`${source}css/**/*.styl`, gulp.series(css));
+
+    gulp.watch(`${source}js/**/*.js`, gulp.series(js));
+
+    gulp.watch(`${dest}assets/css/*.css`, file => {
+        log.info(colors.blue('[INFO] Style has been changed!'));
+        livereload.changed(file);
+    });
+
+    gulp.watch(paths.htmls, file => {
+        log.info(colors.green('[INFO] HTML has been changed!'));
+        livereload.changed(file);
+    });
+
+    gulp.watch(paths.phps, file => {
+        log.info(colors.yellow('[INFO] PHP has been changed!'));
+        livereload.changed(file);
+    });
+}
+
+/* Exported Tasks */
+gulp.task('vendorjs', vendorJs);
+gulp.task('vendorcss', vendorCss);
+gulp.task('js', js);
+gulp.task('lint', lint);
+gulp.task('css', css);
+gulp.task('watch', watchFiles);
+gulp.task('buildVendors', gulp.parallel(vendorCss, vendorJs));
+gulp.task('default', gulp.parallel(css, js));
